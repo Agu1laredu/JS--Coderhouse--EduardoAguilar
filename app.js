1452b3bb7f6b176caaaafd92cc238249c5bf651f
Eduardo Aguilar <agu1laredu@hotmail.com>
2022-05-30 17:45:21 -0300

    main

---
+++ app.js
@@ -0,0 +1,13 @@
+for ( i = 0 ; i <= 5 ; i++) {
+        let x = 1 ;
+        while (x <= 24) {
+                console.log(`Hora del dia ${x}`) ;
+                x++;
+        }
+        console.log(`Fin del dia ${i}`)
+        
+}
+console.log('Fin de la semana');
+
+
+// En este bucle anidamos for y while para contar los dias de la semana (5) y las horas del dia (24); 
