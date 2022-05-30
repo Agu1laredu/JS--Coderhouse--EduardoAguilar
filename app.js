9a3582f85544803107401bc79fb8078c514a20d6
Eduardo Aguilar <agu1laredu@hotmail.com>
2022-05-30 20:00:04 -0300

    Bucle anidados

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 for ( i = 0 ; i <= 5 ; i++) {
         let x = 1 ;
         while (x <= 24) {
-                console.log(`Hora del dia ${x}`) ;
+                console.log(`Hora del dia ${x} hs`) ;
                 x++;
         }
-        console.log(`Fin del dia ${i}`)
+        console.log(`Finaliza un dia, comienza el otro `)
         
 }
 console.log('Fin de la semana');
 
 
 // En este bucle anidamos for y while para contar los dias de la semana (5) y las horas del dia (24); 
+
+
